a1eecdd06a9919e51b6d4f00c5804647372f272c
liu_yan <liu_yan@corp.netease.com>
2015-08-21 17:40:14 +0800

    islider_Demo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".process-percent").animate({"width":"100%"},4000,"linear",function(){
+      $(".loading-page").hide();
+      $("#iSlider").animate({"opacity":"1"},400);
+  });
+
+  //更多滑屏事件
+})
